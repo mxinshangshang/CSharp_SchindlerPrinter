7c42a16107c47cf893fa3fc58cf5cf72e581ddb5
MxinShangshang <jiangmingxin825@live.cn>
2015-09-13 16:02:25 +0800

    CSharp_SchindlerPrinter
    20150913

---
+++ schindlerPrint/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace schindlerPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.模板路径设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.标签内容表格路径设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(119, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(211, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(28, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(302, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始打印";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(361, 25);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.模板路径设置ToolStripMenuItem,
+            this.标签内容表格路径设置ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 模板路径设置ToolStripMenuItem
+            // 
+            this.模板路径设置ToolStripMenuItem.Name = "模板路径设置ToolStripMenuItem";
+            this.模板路径设置ToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.模板路径设置ToolStripMenuItem.Text = "打印模板路径设置";
+            this.模板路径设置ToolStripMenuItem.Click += new System.EventHandler(this.模板路径设置ToolStripMenuItem_Click);
+            // 
+            // 标签内容表格路径设置ToolStripMenuItem
+            // 
+            this.标签内容表格路径设置ToolStripMenuItem.Name = "标签内容表格路径设置ToolStripMenuItem";
+            this.标签内容表格路径设置ToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.标签内容表格路径设置ToolStripMenuItem.Text = "标签内容表格路径设置";
+            this.标签内容表格路径设置ToolStripMenuItem.Click += new System.EventHandler(this.标签内容表格路径设置ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(24, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "打印机：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(24, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "标签ID：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 187);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Schindler Printer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 模板路径设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 标签内容表格路径设置ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ schindlerPrint/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Data.OleDb;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+
+namespace schindlerPrint
+{
+    public partial class Form1 : Form
+    {
+        private string SamplePath;
+        private string ExcelPath;
+        private DataSet myDataSet;
+        private string[,] Prm;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            PrintDocument print = new PrintDocument();
+            string sDefault = print.PrinterSettings.PrinterName;               //默认打印机名
+            comboBox1.Text = sDefault;
+
+            foreach (string sPrint in PrinterSettings.InstalledPrinters)       //获取打印机列表
+            {
+                comboBox1.Items.Add(sPrint);
+                if (sPrint == sDefault)
+                    comboBox1.SelectedIndex = comboBox1.Items.IndexOf(sPrint);
+            }
+        }
+
+        private void 模板路径设置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "请选择打印模板文件所在路径";
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                SamplePath = folderBrowserDialog.SelectedPath;
+            }
+            Properties.Settings.Default.SamplePathSetting = SamplePath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void 标签内容表格路径设置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "请选择标签内容表格所在路径";
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                ExcelPath = folderBrowserDialog.SelectedPath;
+            }
+            Properties.Settings.Default.ExcelPathSetting = ExcelPath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void GetPrn()                                                   //读入Prn内容
+        {
+            SamplePath = Properties.Settings.Default.SamplePathSetting;
+            string path = SamplePath + "/";
+            string[] text = File.ReadAllLines(path + "/" + "area.prn");
+        }
+
+        private void GetExcel()                                              //读入Excel内容
+        {
+            string SamplePath;
+            SamplePath = Properties.Settings.Default.SamplePathSetting;
+            string path = SamplePath + "/";
+            string strCon = " Provider = Microsoft.Jet.OLEDB.4.0 ; Data Source = " + path + "PLC.xls;Extended Properties='Excel 8.0;HDR=NO;IMEX=1;'";    //创建一个数据链接
+            OleDbConnection myConn = new OleDbConnection(strCon);
+            string strCom = " SELECT * FROM [Sheet1$] ";
+            //try
+            //{
+            myConn.Open();
+            OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);                  //打开数据链接，得到一个数据集
+            myDataSet = new DataSet();                                                          //创建一个 DataSet对象
+            myCommand.Fill(myDataSet, "[Sheet1$]");                                             //得到自己的DataSet对象
+            myConn.Close();                                                                     //关闭此数据链接
+
+            Prm = new string[myDataSet.Tables[0].Rows.Count, 8];
+
+            for (int i = 0; i < myDataSet.Tables[0].Rows.Count; i++)                            //读取Sheet1里的配置信息
+            {
+                Prm[i, 0] = myDataSet.Tables[0].Rows[i].ItemArray[2].ToString();            //配置物料的名称
+                Prm[i, 1] = myDataSet.Tables[0].Rows[i].ItemArray[1].ToString();            //配置物料的ID号
+                Prm[i, 2] = myDataSet.Tables[0].Rows[i].ItemArray[3].ToString();            //配置物料第一命令地址
+                Prm[i, 3] = myDataSet.Tables[0].Rows[i].ItemArray[4].ToString();            //配置物料第二命令地址
+                Prm[i, 4] = myDataSet.Tables[0].Rows[i].ItemArray[5].ToString();            //配置物料第三命令地址
+                Prm[i, 5] = myDataSet.Tables[0].Rows[i].ItemArray[6].ToString();            //配置物料测试完成确认地址
+                Prm[i, 6] = myDataSet.Tables[0].Rows[i].ItemArray[7].ToString();            //配置物料线束连接提示
+                Prm[i, 7] = myDataSet.Tables[0].Rows[i].ItemArray[8].ToString();            //配置物料断路器闭合提示
+            }
+            //}
+            //catch (Exception e)
+            //{
+            //    MessageBox.Show(e.Message);
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //BarCodePrint lpt = new BarCodePrint();
+            //string mycommanglines = File.ReadAllText("area.prn");
+            //lpt.Open();
+            //lpt.Write(mycommanglines);
+            //lpt.Close();
+
+            string cmd = File.ReadAllText("area.prn");
+
+            ZebraPrintHelper.SendFileToPrinter(comboBox1.Text, "area.prn");
+            //if (!printer.Open())
+            //{
+            //    MessageBox.Show("未能连接打印机，请确认打印机是否安装正确并接通电源。");
+            //    return;
+            //}
+            //printer.Write(cmd);
+            //if (!printer.Close())
+            //{
+            //    MessageBox.Show("未能关闭与打印机之间的连接，这可能意味着严重的错误，请重启电脑及打印机。");
+            //    return;
+            //}
+        }
+    }
+}
